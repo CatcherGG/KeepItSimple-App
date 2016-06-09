c57037c2aea6529b0c28ee6b71178a35fe8edd2a
Ofri Massad <ofri.masad@emc.com>
2016-06-09 23:20:45 +0300

    added new list style, implemented click feel with changing images

--- app/src/main/java/com/productions/keep/keepitsimple/AlarmActivity.java
+++ app/src/main/java/com/productions/keep/keepitsimple/AlarmActivity.java
@@ -1,7 +1,6 @@
 package com.productions.keep.keepitsimple;
 
 import android.app.Activity;
-import android.app.ActivityOptions;
 import android.app.AlarmManager;
 import android.app.AlertDialog;
 import android.app.PendingIntent;
@@ -9,20 +8,17 @@ import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.graphics.drawable.BitmapDrawable;
 import android.media.Ringtone;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Vibrator;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ListView;
 
 public class AlarmActivity extends Activity {
     private ImageButton stopAlarm;
@@ -37,6 +33,8 @@ public class AlarmActivity extends Activity {
     private Vibrator vibrator;
     private DetectedActivitiesIntentService detectionService;
     private boolean mBounded;
+    private AlertDialog dialog;
+    private AlertDialog.Builder dialogBuilder;
 
     public static AlarmActivity instance() {
         return inst;
@@ -49,13 +47,7 @@ public class AlarmActivity extends Activity {
         Intent mIntent = new Intent(this, DetectedActivitiesIntentService.class);
         bindService(mIntent, mConnection, BIND_AUTO_CREATE);
         inst = this;
-        if (ringtone==null|| !ringtone.isPlaying()) {
-            counter++;
-            if (counter % 2 == 0) {
-                stopAlarm.setImageResource(R.drawable.alarm2);
-            } else {
-                stopAlarm.setImageResource(R.drawable.alarm1);
-            }
+        if (ringtone == null || !ringtone.isPlaying()) {
             startAlarm();
         }
     }
@@ -77,14 +69,14 @@ public class AlarmActivity extends Activity {
     ServiceConnection mConnection = new ServiceConnection() {
 
         public void onServiceDisconnected(ComponentName name) {
-            Log.d("WithoutChild","disconnected the service");
+            Log.d("WithoutChild", "disconnected the service");
             //Toast.makeText(WithoutChild.this, "Service is disconnected", Toast.LENGTH_SHORT).show();
             mBounded = false;
             detectionService = null;
         }
 
         public void onServiceConnected(ComponentName name, IBinder service) {
-            Log.d("WithoutChild","connected the service");
+            Log.d("WithoutChild", "connected the service");
             //Toast.makeText(WithoutChild.this, "Service is connected", Toast.LENGTH_SHORT).show();
             mBounded = true;
             DetectedActivitiesIntentService.LocalBinder mLocalBinder = (DetectedActivitiesIntentService.LocalBinder) service;
@@ -105,64 +97,150 @@ public class AlarmActivity extends Activity {
         stopAlarm = (ImageButton) findViewById(R.id.button5);
         childNotWithMe = (ImageButton) findViewById(R.id.button6);
         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        stopAlarm.setOnClickListener(new View.OnClickListener() {
+
+        dialogBuilder = createDialog();
+
+        stopAlarm.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                stopAlarm();
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    stopAlarm.setImageResource(R.drawable.alarm_nope_shade);
+                    Log.d("AlarmActivity", "stop alarm");
+                    finish();
+                    // Do what you want
+                    return true;
+                } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    stopAlarm.setImageResource(R.drawable.alarm_nope_pushed);
+                    return true;
+                }
+                return false;
+            }
+        });
+        childNotWithMe.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    childNotWithMe.setImageResource(R.drawable.alarm_mute_shade);
+                    Log.d("AlarmActivity", "clicked on child not with me");
+                    stopAlarm();
+                    dialog = dialogBuilder.show();
+                    // Do what you want
+                    return true;
+                } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    childNotWithMe.setImageResource(R.drawable.alarm_mute_pushed);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private AlertDialog.Builder createDialog(){
+        LayoutInflater inflater = (LayoutInflater) getSystemService(getApplicationContext().LAYOUT_INFLATER_SERVICE);
+        final View formElementsView = inflater.inflate(R.layout.activity_other,
+                null, false);
+        final ImageButton hour1 = (ImageButton) formElementsView.findViewById(R.id.hour1);
+        final ImageButton hour8 = (ImageButton) formElementsView.findViewById(R.id.hour8);
+        final ImageButton hour12 = (ImageButton) formElementsView.findViewById(R.id.hour12);
+        final ImageButton hour24 = (ImageButton) formElementsView.findViewById(R.id.hour24);
+        final ImageButton ops = (ImageButton) formElementsView.findViewById(R.id.ops);
+        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(AlarmActivity.this).setView(formElementsView);
+        dialogBuilder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
                 finish();
             }
         });
-        childNotWithMe.setOnClickListener(new View.OnClickListener() {
+        hour1.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                stopAlarm();
-                Log.d("AlarmActivity", "clicked on child not with me");
-                String names[] = {"1 Hour", "8 Hours", "12 Hours", "24 Hours"};
-
-                final CharSequence[] items = {"1 Hour", "8 Hours", "12 Hours", "24 Hours","Ops..."};
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(AlarmActivity.this,R.style.AboutDialog);
-                builder.setTitle("Mute for:");
-                builder.setItems(items, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int item) {
-                        Log.d("AlarmActivity","choose "+items[item].toString());
-                        long seconds = -1;
-                        switch (item){
-                            case 0:
-                                seconds = 60*60;
-                                break;
-                            case 1:
-                                seconds = 60*60*8;
-                                break;
-                            case 2:
-                                seconds = 60*60*12;
-                                break;
-                            case 3:
-                                seconds = 60*60*24;
-                                break;
-                        }
-                        detectionService.stopDetectionForPeriod(seconds);
-                        finish();
-                    }
-                });
-                builder.show();
-
-
-                /*AlertDialog.Builder alertDialog = new AlertDialog.Builder(AlarmActivity.this);
-                LayoutInflater inflater = getLayoutInflater();
-                View convertView = (View) inflater.inflate(R.layout.custom, null);
-                alertDialog.setView(convertView);
-                alertDialog.setTitle("Mute for:");
-                ListView lv = (ListView) convertView.findViewById(R.id.listView1);
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(AlarmActivity.this, android.R.layout.simple_list_item_1, names);
-                lv.setAdapter(adapter);
-                alertDialog.show();*/
-                //Intent intent = new Intent(AlarmActivity.this,WithoutChild.class);
-                //startActivity(intent);
-                //finish();
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    hour1.setImageResource(R.drawable.hour1_shade);
+                    Log.d("AlarmActivity", "mute for 1 hour");
+                    detectionService.stopDetectionForPeriod(60 * 60 * 1);
+                    dialog.dismiss();
+                    finish();
+                    // Do what you want
+                    return true;
+                } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    hour1.setImageResource(R.drawable.hour1_pushed);
+                    return true;
+                }
+                return false;
             }
         });
+        hour8.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    hour8.setImageResource(R.drawable.hour8_shade);
+                    Log.d("AlarmActivity", "mute for 8 hours");
+                    detectionService.stopDetectionForPeriod(60 * 60 * 8);
+                    dialog.dismiss();
+                    finish();
+                    // Do what you want
+                    return true;
+                } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    hour8.setImageResource(R.drawable.hour8_pushed);
+                    return true;
+                }
+                return false;
+            }
+        });
+        hour12.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    hour12.setImageResource(R.drawable.hour12_shade);
+                    Log.d("AlarmActivity", "mute for 12 hours");
+                    detectionService.stopDetectionForPeriod(60 * 60 * 12);
+                    dialog.dismiss();
+                    finish();
+                    // Do what you want
+                    return true;
+                } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    hour12.setImageResource(R.drawable.hour12_pushed);
+                    return true;
+                }
+                return false;
+            }
+        });
+        hour24.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    hour24.setImageResource(R.drawable.hour24_shade);
+                    Log.d("AlarmActivity", "mute for 24 hours");
+                    detectionService.stopDetectionForPeriod(60 * 60 * 24);
+                    dialog.dismiss();
+                    finish();
+                    // Do what you want
+                    return true;
+                } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    hour24.setImageResource(R.drawable.hour24_pushed);
+                    return true;
+                }
+                return false;
+            }
+        });
+        ops.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    ops.setImageResource(R.drawable.back_shade);
+                    dialog.dismiss();
+                    finish();
+                    // Do what you want
+                    return true;
+                } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    ops.setImageResource(R.drawable.back_pushed);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        return dialogBuilder;
     }
 
     @Override
